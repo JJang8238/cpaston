122e1dc8acfa9d9658f06ba9e431b08f18f43a77
yunsuk123 <yangys0110@gmail.com>
2025-10-14 23:01:52 +0900

    main, DB.java 수정

--- Ball/src/main/java/util/DBConnection.java
+++ Ball/src/main/java/util/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class DBConnection {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/balldb?serverTimezone=UTC";  // ✅ DB 이름
+        String url = "jdbc:mysql://localhost:3306/grade_DB?serverTimezone=UTC";  // ✅ DB 이름
         String user = "root";    // ✅ MySQL 사용자 이름
-        String pass = "8238";    // ✅ MySQL 비밀번호
+        String pass = "1234";    // ✅ MySQL 비밀번호
         return DriverManager.getConnection(url, user, pass);
     }
 }
